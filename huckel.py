d4c71d8a25193f657d03814575529e1d620ae93d
yuki <amagai05.14@gmail.com>
2019-05-19 22:57:00 +0900

    add energy

--- huckel.py
+++ huckel.py
@@ -2,7 +2,10 @@ import sympy
 from sympy import *
 
 x = Symbol('x')
-a = Matrix([[2,x], [4, -1]])
-print(a)
-answer = solve(a.det(),x)
-print(answer)
+a = Symbol('a')
+b = Symbol('b')
+
+matrix = Matrix([[-x,1,0,0], [1,-x,1,0],[0,1,-x,1],[0,0,1,-x]])
+answer = solve(matrix.det(), x)
+energy = [a+X*b for X in answer]
+print(energy)
